4a68bad8a7c00ceb97d56243b9c10cc03ffade0a
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-13 22:35:33 +0200

    dark mode

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 'use strict';
 const button = document.querySelector('.slider');
 const body = document.querySelector('body');
+const summerText = document.querySelector('.header__subtitle')
+const winterText = document.querySelector('.header__subtitle2')
 
 const changeMode = () =>{
     body.classList.toggle('dark')
+    summerText.classList.toggle('hidden')
+    if(body.classList.contains('dark')){
+        winterText.classList.remove('hidden')
+    }else{
+        winterText.classList.add('hidden')
+    }
 }
 
 button.addEventListener('click', changeMode)
