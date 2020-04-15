d819288fee452c6d40c327889afc1da153e6999b
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-15 17:07:32 +0200

    fixing classes

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 'use strict';
 const button = document.querySelector('.slider');
 const body = document.querySelector('body');
-const summerText = document.querySelector('.header__subtitle')
-const winterText = document.querySelector('.header__subtitle2')
+const text = document.querySelector('.header__subtitle')
 const sun = document.querySelector('.sun')
 const flexo = document.querySelector('.flexo')
 
 const changeMode = () =>{
     body.classList.toggle('dark')
-    summerText.classList.toggle('hidden')
     changeElements()
 }
 const changeElements =()=>{
     if(body.classList.contains('dark')){
-        winterText.classList.remove('hidden')
+        text.innerHTML='Reality'
+        text.classList.remove('yellow')
+        text.classList.add('orange')
         sun.classList.add('disappear')
         flexo.classList.remove('disappear')
     }else{
-        winterText.classList.add('hidden')
-       flexo.classList.add('disappear')
-       sun.classList.remove('disappear')
+        text.innerHTML='Expectations'
+        text.classList.add('yellow')
+        flexo.classList.add('disappear')
+        sun.classList.remove('disappear')
     }
 }
 
